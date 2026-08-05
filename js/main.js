0b527739a138d0eff71bba9505613f8615f61031
Saša Radovanović <sasa@radovanovic.net>
2026-08-05 12:59:35 +0200

    changes the js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+// // Navbar Toggle
+// burgerMenu = document.querySelector('.burger-menu')
+// menuResponsive = document.querySelector('.menu-responsive')
+// links = document.querySelectorAll('.link')
+
+// const toggleNavbar = () => {
+//   burgerMenu.classList.toggle('active')
+//   menuResponsive.classList.toggle('active')
+// }
+
+// const removeNavbar = () => {
+//   burgerMenu.classList.remove('active')
+//   menuResponsive.classList.remove('active')
+// }
+
+// burgerMenu.addEventListener('click', toggleNavbar)
+// links.forEach((link) => {
+//   link.addEventListener('click', removeNavbar)
+// })
+
+// const navLogo = document.querySelector('.nav-logo')
+
+// navLogo.addEventListener('click', removeNavbar)
+
+// // // Toggle Nav Variant
+// const nav = document.querySelector('nav')
+
+// const navDark = () => {
+//   nav.classList.add('nav-dark')
+// }
+
+// const navLight = () => {
+//   nav.classList.remove('nav-dark')
+// }
+
+// // // Hero Trigger
+// ScrollTrigger.create({
+//   // markers: true,
+//   trigger: '.hero',
+//   start: 'top bottom',
+//   endTrigger: '.hero',
+//   end: 'bottom top',
+//   onEnter: () => navLight(),
+//   onLeave: () => navDark(),
+//   onEnterBack: () => navLight(),
+// })
+
 // Navbar Toggle
 burgerMenu = document.querySelector('.burger-menu')
 menuResponsive = document.querySelector('.menu-responsive')
@@ -6,6 +53,11 @@ links = document.querySelectorAll('.link')
 const toggleNavbar = () => {
   burgerMenu.classList.toggle('active')
   menuResponsive.classList.toggle('active')
+  
+  // Remove nav-dark class when opening the responsive navbar
+  if (menuResponsive.classList.contains('active')) {
+    nav.classList.remove('nav-dark')
+  }
 }
 
 const removeNavbar = () => {
